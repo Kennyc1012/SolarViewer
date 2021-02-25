59d87e31711a02bd01371d615e6b29ec390bac2d
Kenny <kennyc1012@gmail.com>
2021-02-24 20:12:41 -0500

    Update stuff

--- data-store/src/main/java/com/kennyc/data_store/PreferenceStore.kt
+++ data-store/src/main/java/com/kennyc/data_store/PreferenceStore.kt
@@ -1,33 +1,19 @@
 package com.kennyc.data_store
 
 import android.content.Context
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.emptyPreferences
-import androidx.datastore.preferences.core.preferencesKey
-import androidx.datastore.preferences.createDataStore
 import com.kennyc.solarviewer.data.LocalSettings
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.map
-import java.io.IOException
+import kotlinx.coroutines.flow.flowOf
 
 class PreferenceStore(context: Context) : LocalSettings {
 
-    private val store = context.createDataStore("PreferenceStore")
 
     override suspend fun store(key: String, value: String) {
-        store.edit { it[preferencesKey(key)] = value }
+        // TODO Convert to RX
     }
 
     override fun getStoredString(key: String, defaultValue: String): Flow<String> {
-        return store.data
-            .catch { exception ->
-                if (exception is IOException) {
-                    emit(emptyPreferences())
-                } else {
-                    throw exception
-                }
-            }
-            .map { it[preferencesKey<String>(key)] ?: defaultValue }
+        // TODO Convert to RX
+        return flowOf()
     }
 }
