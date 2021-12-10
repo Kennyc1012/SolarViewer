b669274a2d7081dec1d2c29bd79382d628b9c393
Kenny <kennyc1012@gmail.com>
2021-12-09 19:18:13 -0500

    Fix ViewModel Race condition
    There is a race condition where the data from the RX calls might not be delivered in time to render the UI

--- app/src/main/java/com/kennyc/solarviewer/SystemsViewModel.kt
+++ app/src/main/java/com/kennyc/solarviewer/SystemsViewModel.kt
@@ -7,6 +7,7 @@ import com.kennyc.solarviewer.data.SolarRepository
 import com.kennyc.solarviewer.data.model.SolarSystem
 import com.kennyc.solarviewer.utils.RxUtils.observeChain
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 import java.util.*
 import javax.inject.Inject
 
@@ -15,6 +16,8 @@ class SystemsViewModel @Inject constructor(
     private val repo: SolarRepository,
     private val clock: Clock
 ) : ViewModel() {
+
+    private val disposables = CompositeDisposable()
 
     val currentTime: Date get() = clock.currentDate()
 
@@ -25,31 +28,39 @@ class SystemsViewModel @Inject constructor(
     val selectedDate = repo.selectedDate()
 
     init {
-        systems.withLatestFrom(localSettings
+        val d =localSettings
             .getStoredString(KEY_LAST_USED_SYSTEM, DEFAULT_VALUE)
-            .toObservable(),
-            { systems, lastUsed ->
-                lastUsed
-                    // Get last stored system
-                    .takeIf { id -> id != DEFAULT_VALUE }
-                    ?.let { id ->
-                        // Return the system with the same id
-                        systems.firstOrNull { s ->
-                            s.id == id
-                        }
-                        // If not found return first item
-                    } ?: systems.first()
-            })
-            .observeChain()
+            .toObservable()
+            .flatMap { lastUsed ->
+                repo.getSolarSystems()
+                    .map { systems ->
+                        lastUsed
+                            // Get last stored system
+                            .takeIf { id -> id != DEFAULT_VALUE }
+                            ?.let { id ->
+                                // Return the system with the same id
+                                systems.firstOrNull { s ->
+                                    s.id == id
+                                }
+                                // If not found return first item
+                            } ?: systems.first()
+                    }
+            }.observeChain()
             .subscribe {
                 repo.currentSystem = it
             }
 
         setNewDate(currentTime)
+        disposables.add(d)
     }
 
     fun setNewDate(newDate: Date) {
         repo.currentDate = newDate
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposables.clear()
     }
 }
 
