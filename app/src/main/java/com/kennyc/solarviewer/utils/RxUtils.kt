21c3aba3779cc30364ee998c6663ed5b7367d130
Kenny <kennyc1012@gmail.com>
2021-12-09 19:11:34 -0500

    Remove Unused dependencies

--- app/src/main/java/com/kennyc/solarviewer/utils/RxUtils.kt
+++ app/src/main/java/com/kennyc/solarviewer/utils/RxUtils.kt
@@ -1,26 +1,11 @@
 package com.kennyc.solarviewer.utils
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.LiveDataReactiveStreams
-import androidx.lifecycle.toLiveData
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.*
 import io.reactivex.rxjava3.schedulers.Schedulers
 import io.reactivex.rxjava3.subjects.Subject
 
 object RxUtils {
-
-    fun <T> Flowable<T>.asLiveData(): LiveData<T> {
-        return LiveDataReactiveStreams.fromPublisher(this)
-    }
-
-    fun <T> Subject<T>.asLiveData(): LiveData<T> =
-        toFlowable(BackpressureStrategy.LATEST).toLiveData()
-
-    fun <T> Observable<T>.asLiveData(): LiveData<T> =
-        toFlowable(BackpressureStrategy.LATEST).toLiveData()
-
-    fun <T> Single<T>.asLiveData(): LiveData<T> = toFlowable().asLiveData()
 
     fun <T> Observable<T>.observeChain(): Observable<T> =
         subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
